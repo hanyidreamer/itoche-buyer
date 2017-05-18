1a01bb6eaef88df4d9d349b3a29a124cc27f755b
hanyidremer <420615947@qq.com>
2017-05-18 21:53:56 +0800

    170518 commit

--- itoche-buyer-service/src/main/java/com/itoche/buyer/impl/TestServiceImpl.java
+++ itoche-buyer-service/src/main/java/com/itoche/buyer/impl/TestServiceImpl.java
@@ -1,8 +1,10 @@
 package com.itoche.buyer.impl;
 
+import com.itoche.buyer.service.TestService;
+
 /**
  * Created by Administrator on 2017/5/14 0014.
  */
-public class TestServiceImpl
+public class TestServiceImpl implements TestService
 {
 }
